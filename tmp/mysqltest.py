c3808be2696707838056d16727a764ef779b0e14
wxiang54 <willxiang1999@gmail.com>
2019-05-08 14:19:28 -0400

    merge

--- tmp/mysqltest.py
+++ tmp/mysqltest.py
@@ -1,13 +1,17 @@
 import MySQLdb
+
 cnx = MySQLdb.connect(user='root', password='root',
                               host='localhost', database='storedb')
 cursor = cnx.cursor()
+
 query = ("SELECT * FROM item_stock")
 cursor.execute(query)
 for item_stock in cursor:
     print(item_stock)
 cursor.execute("SHOW TABLES")
+
 for table in cursor.fetchall():
     print(table)
+
 cursor.close()
 cnx.close()
