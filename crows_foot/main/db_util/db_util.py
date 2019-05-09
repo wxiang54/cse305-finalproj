769ef7176183b0e734295a746ad7d944537063b5
wxiang54 <willxiang1999@gmail.com>
2019-05-09 01:16:12 -0400

    add icons to static/images/item_icons and prep migration script for item insertion

--- crows_foot/main/db_util/db_util.py
+++ crows_foot/main/db_util/db_util.py
@@ -1,4 +1,5 @@
 from django.db import connection
+import os
 
 '''
 def my_custom_sql(self):
@@ -10,5 +11,39 @@ def my_custom_sql(self):
     return row
 '''
 
-def insert_item(itemstock_id):
-    pass
+schema_path = "main/db_util/main_schema.sql"
+
+def parse_schema():
+    #print(os.path.dirname(os.path.abspath(__file__)))
+    schema_sql = []
+
+    with open(schema_path, 'r') as file:
+        data = file.read().split("\n")
+        reading_sql = False
+        cur_statement = ""
+        for line in data:
+            line = line.strip()
+            if line == '':
+                continue
+            if line == "/* START */":
+                reading_sql = True
+                continue
+            if line == "/* END */":
+                reading_sql = False
+                schema_sql.append(cur_statement)
+                cur_statement = ""
+                continue
+            if reading_sql:
+                cur_statement += line + " "
+
+    return schema_sql
+
+#def parse_insertion_items()
+
+def insert_item(itemstock_id, was_bought=False):
+    with connection.cursor() as cursor:
+        cursor.execute("INSERT INTO item VALUES (%s, %s)", [itemstock_id, was_bought])
+
+
+if __name__ == "__main__":
+    print(parse_schema())
